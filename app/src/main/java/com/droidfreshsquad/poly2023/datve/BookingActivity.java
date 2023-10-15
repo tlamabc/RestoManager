8e1c7a4a229beef01e59b05674a0ee52eed83b3a
nhacnguyen03 <nhacntpd06031@fpt.edu.vn>
2023-10-15 21:03:35 +0700

    nhac commit khứ hồi

--- app/src/main/java/com/droidfreshsquad/poly2023/datve/BookingActivity.java
+++ app/src/main/java/com/droidfreshsquad/poly2023/datve/BookingActivity.java
@@ -1,33 +1,22 @@
 package com.droidfreshsquad.poly2023.datve;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.CompoundButton;
-
 import com.droidfreshsquad.poly2023.R;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class BookingActivity extends AppCompatActivity {
-    private TextView edtthoigian, tvNgayVe, ngayve01, tvsohanhkhach;
+    private TextView edtthoigian, tvNgayVe, ngayve01;
     private Switch swKhuhoi;
-    BottomSheetDialog dialog;
-    private TextView tvNumberLon, tvNumberTreEm, tvNumberEmBe;
-    private Button btnPlusLon, btnPlusTreEm, btnPlusEmBe, btnMinusLon, btnMinusTreEm, btnMinusEmBe, btnSoKhach;
-    private int numberLon = 1, numberTreEm = 0, numberEmBe = 0;
     private boolean isDatePickerVisible = false;
 
     @SuppressLint("MissingInflatedId")
@@ -40,116 +29,6 @@ public class BookingActivity extends AppCompatActivity {
         tvNgayVe = (TextView) findViewById(R.id.tvNgayVe);
         ngayve01 = (TextView) findViewById(R.id.ngayve01);
         edtthoigian = (TextView) findViewById(R.id.edtthoigian);
-        tvsohanhkhach = (TextView) findViewById(R.id.tvsohanhkhach);
-
-
-// Tạo bottom sheet dialog chọn số lượng người
-        dialog = new BottomSheetDialog(this);
-        // Tạo giao diện cho bottom sheet dialog
-        View viewDialog = LayoutInflater.from(this).inflate(R.layout.dialog_so_nguoi, null);
-        dialog.setContentView(viewDialog);
-        tvsohanhkhach.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.show(); // Hiển thị bottom sheet dialog
-            }
-        });
-        // id người lớn
-        tvNumberLon = (TextView) viewDialog.findViewById(R.id.tvNumberLon);
-        btnPlusLon = (Button) viewDialog.findViewById(R.id.btnPlusLon);
-        btnMinusLon = (Button) viewDialog.findViewById(R.id.btnMinusLon);
-        //id trẻ em
-        tvNumberTreEm = (TextView) viewDialog.findViewById(R.id.tvNumberTreEm);
-        btnPlusTreEm = (Button) viewDialog.findViewById(R.id.btnPlusTreEm);
-        btnMinusTreEm = (Button) viewDialog.findViewById(R.id.btnMinusTreEm);
-        // id em bé
-        tvNumberEmBe = (TextView) viewDialog.findViewById(R.id.tvNumberEmBe);
-        btnPlusEmBe = (Button) viewDialog.findViewById(R.id.btnPlusEmBe);
-        btnMinusEmBe = (Button) viewDialog.findViewById(R.id.btnMinusEmBe);
-        //id button xác nhận
-        btnSoKhach = (Button) viewDialog.findViewById(R.id.btnSoKhach);
-
-
-        //tăng biến người lớn
-        btnPlusLon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberLon++;
-                tvNumberLon.setText(String.valueOf(numberLon));
-            }
-        });
-        //giảm biến người lớn
-        btnMinusLon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numberLon > 1) {
-                    numberLon--; // Giảm biến số chỉ khi nó lớn hơn 0
-                    tvNumberLon.setText(String.valueOf(numberLon));
-                }
-            }
-        });
-        //tăng biến trẻ em
-        btnPlusTreEm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberTreEm++;
-                tvNumberTreEm.setText(String.valueOf(numberTreEm));
-            }
-        });
-        //giảm biến trẻ em
-        btnMinusTreEm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numberTreEm > 0) {
-                    numberTreEm--;
-                    tvNumberTreEm.setText(String.valueOf(numberTreEm));
-                }
-            }
-        });
-        //tăng biến em bé
-        btnPlusEmBe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberEmBe++;
-                tvNumberEmBe.setText(String.valueOf(numberEmBe));
-            }
-        });
-        //giảm biến em bé
-        btnMinusEmBe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numberEmBe > 0) {
-                    numberEmBe--;
-                    tvNumberEmBe.setText(String.valueOf(numberEmBe));
-                }
-            }
-        });
-
-        // button xác nhận, đóng dialog
-        btnSoKhach.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
-        //Thêm sự kiện cho bottom sheet dialog
-        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                StringBuilder numbersText = new StringBuilder();
-                if (numberLon > 0) {
-                    numbersText.append(numberLon).append(" người lớn").append("\n");
-                }
-                if (numberTreEm > 0) {
-                    numbersText.append(", ").append(numberTreEm).append(" trẻ em").append("\n");
-                }
-                if (numberEmBe > 0) {
-                    numbersText.append(", ").append(numberEmBe).append(" em bé").append("\n");
-                }
-                tvsohanhkhach.setText(numbersText.toString());//in ra textView
-            }
-        });
 
 
 //bấm vào textview chuyển sang màng hình chọn ngày
